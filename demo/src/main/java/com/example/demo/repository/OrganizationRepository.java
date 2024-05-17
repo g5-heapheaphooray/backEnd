8a0865522cbc699dbedc95fbd6d279e6f5977b1e
ppv-1 <virus9320@gmail.com>
2024-05-17 20:06:00 +0800

    ngjac3

--- demo/src/main/java/com/example/demo/repository/OrganizationRepository.java
+++ demo/src/main/java/com/example/demo/repository/OrganizationRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrganizationRepository extends JpaRespository<Organization, String> {
+public interface OrganizationRepository extends JpaRepository<Organization, String> {
     
 }
 
--- demo/src/main/java/com/example/demo/repository/RewardRepository.java
+++ demo/src/main/java/com/example/demo/repository/RewardRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RewardRepository extends JpaRespository<Reward, Integer> {
+public interface RewardRepository extends JpaRepository<Reward, Integer> {
     
 }
