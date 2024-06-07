2997f545e6c9986a281514afcc46d7528db346d4
WastefulPines <126472165+WastefulPines@users.noreply.github.com>
2024-06-07 20:23:06 +0800

    Many changes made

--- demo/src/main/java/com/example/demo/repository/EventRepository.java
+++ demo/src/main/java/com/example/demo/repository/EventRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Integer> {
+public interface EventRepository extends JpaRepository<Event, String> {
     
 }
