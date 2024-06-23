10406abc4ea7e7d0d2215a8fb03d51d80d16ab1a
WastefulPines <126472165+WastefulPines@users.noreply.github.com>
2024-06-24 01:11:59 +0800

    added find events by org

--- demo/src/main/java/com/example/demo/repository/EventRepository.java
+++ demo/src/main/java/com/example/demo/repository/EventRepository.java
@@ -3,12 +3,15 @@ package com.example.demo.repository;
 import com.example.demo.model.Event;
 import com.example.demo.model.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, String> {
-    List<Event> findByOrganisation(String orgId);
+    @Query("SELECT e FROM Event e WHERE e.organisation = :orgId")
+    List<Event> findByOrganisation(@Param("orgId") String orgId);
 
     List<Event> findByDateGreaterThanEqual(LocalDate d);
 
