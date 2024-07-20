52d929c5a119296891ab24d7f13da93e401cb61a
ju57_4_8361nn3r <xuan2103yyx@gmail.com>
2024-07-20 15:44:30 +0800

    pls

--- demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -54,6 +54,7 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<Object> loginUser(@RequestBody AuthenticationDTO authenticationDTO) {
         System.out.println("hello new new new");
+        System.out.println("wruu");
         User user = userService.authenticateUser(authenticationDTO.getEmail(), authenticationDTO.getPassword());
         if (user != null) {
             String jwtToken = jwtService.generateToken(user);
